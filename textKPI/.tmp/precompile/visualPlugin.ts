2fdcdc0a712cea9e87df8b75c71d38abf1da36c1
EricDong <eric.dong@outlook.com>
2018-03-14 21:23:39 +1000

    install d3

---
+++ textKPI/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var textKPIB6F7AC98CDD04F1AA0FD0BD21EDD4511_DEBUG = {
+        name: 'textKPIB6F7AC98CDD04F1AA0FD0BD21EDD4511_DEBUG',
+        displayName: 'textKPI',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.9.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.textKPIB6F7AC98CDD04F1AA0FD0BD21EDD4511.Visual(options),
+        custom: true
+    };
+}
